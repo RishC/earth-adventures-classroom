4c6769663b81088d69de382a41f41dbf9b837c25
Chase Meyer <chasemeyer3@gmail.com>
2019-05-15 22:32:15 -0700

    added folders and some front end but not working for some reason

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { CoreModule } from './core/core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
     DashboardComponent,
-    
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DashboardModule
+    DashboardModule,
+    CoreModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
