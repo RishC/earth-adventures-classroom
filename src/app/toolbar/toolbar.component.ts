84ed975592a6149cb7e022c5e6758d1b32dd1cec
Chase Meyer <chasemeyer3@gmail.com>
2019-05-25 22:31:47 -0700

    Updated main navbar and added new navbar to course description page

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,8 +1,11 @@
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './../core/services/user.service';
 import { AuthService } from 'src/app/core/services/auth.service';
-import { Router } from '@angular/router';
-
+import { CourseService } from '../dashboard/course.service';
+import { Course } from '../core/data/course';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,14 +13,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
-
+  courses$: Observable<Course[]>;
+  selectedId: number;
   isTeacher = false;
+
   constructor(private userService: UserService,
     private authService: AuthService,
-    private router: Router) { }
+    private router: Router,
+    private route: ActivatedRoute,
+    private courseService : CourseService) { }
 
   ngOnInit() {
     this.userService.currentUser.subscribe(user => this.isTeacher = user.type === 'teacher');
+    this.courses$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        this.selectedId = +params.get('id');
+        return this.courseService.getCourses();
+      })
+    );
   }
 
   logout() {
